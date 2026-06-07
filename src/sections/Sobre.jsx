7ecda24a4f7a1591c5a6e3eb00c6833f37462abd
Lucas-MAP <luckdev.98@gmail.com>
2026-06-07 14:43:56 -0300

    ajuste sobre mim

--- src/sections/Sobre.jsx
+++ src/sections/Sobre.jsx
@@ -2,39 +2,35 @@ function Sobre() {
   return (
     <section className="sobre">
       <div className="container">
+        <p className="section-tag">Quem sou</p>
 
-        <p className="section-tag">
-          Quem sou
-        </p>
-
-        <h2 className="section-title">
-          SOBRE MIM
-        </h2>
+        <h2 className="section-title">SOBRE MIM</h2>
 
         <div className="line"></div>
 
         <p className="sobre-text">
-          Sou graduada em História pela Universidade Federal do Tocantins,
-          especialista em Ensino de Geografia, Meio Ambiente e História,
-          especialista em História e Cultura Afro-Brasileira e mestre em
-          História Social.
-
-          <br /><br />
-
-          Atuo nas áreas de educação, pesquisa, formação docente,
-          tutoria acadêmica e produção científica, contribuindo para o
-          desenvolvimento de projetos educacionais e acadêmicos.
-
-          <br /><br />
-
-          Tenho experiência com ensino, orientação de pesquisas,
-          elaboração de materiais didáticos, avaliação pedagógica
-          e formação continuada.
+          Sou historiadora, professora e consultora educacional com foco em
+          Formação Continuada, Ensino de História, Políticas Educacionais e
+          Educação para as Relações Étnico-Raciais (ERER).
+          <br />
+          <br />
+          Mestre em História pela UNIFESSPA e licenciada em História pela UFT,
+          com especializações em Gestão Educacional, História e Cultura
+          Afro-Brasileira e Ensino de Geografia, Meio Ambiente e História. Atuei
+          como coordenadora de pesquisa e extensão na UFT e como coordenadora de
+          linha de pesquisa na UFPA; assistente editorial na Revista Escritas do
+          Tempo do Programa de Pós-Graduação em História da UNIFESSPA e
+          professora da rede municipal de Educação de Ipueiras/Tocantins.
+          <br />
+          <br />
+          Atualmente tenho atuado de forma remota como tutora no CNCA/SP (FGV) e
+          como avaliadora pedagógica no Programa Multiplica SP (Fundação
+          Vanzolini/EFAPE). Acredito em uma educação comprometida com a
+          diversidade, a memória e a equidade.
         </p>
-
       </div>
     </section>
-  )
+  );
 }
 
-export default Sobre
+export default Sobre;
